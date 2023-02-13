166bf1d744d337bf0b66b86d8c36a48692f1bc95
Amyhds <dasomhan524@gmail.com>
2023-02-14 00:35:11 +0900

    finish boj_1011

--- src/week5/boj_1011.java
+++ src/week5/boj_1011.java
@@ -1,10 +1,49 @@
 package week5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class boj_1011 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(bf.readLine());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			int dist = y - x;
+			int d = (int) Math.sqrt(dist);
+			if (dist == d * d) {
+				System.out.println(2 * d - 1);
+			} else if (dist <= d * d + d) {
+				System.out.println(2 * d);
+			} else {
+				System.out.println(2 * d + 1);
+			}
+		}
 	}
 
 }
+// https://cocoon1787.tistory.com/622
+//이동거리 1 => 작동횟수 1
+// 이동거리 2 => 작동횟수 2
+// 이동거리 3 => 작동횟수 3
+//이동거리 4 => 작동횟수 3
+// 이동거리 5 => 작동횟수 4
+// 이동거리 6 => 작동횟수 4
+// 이동거리 7 => 작동횟수 5
+// 이동거리 8 => 작동횟수 5
+//이동거리 9 => 작동횟수 5
+// 이동거리 10 => 작동횟수 6
+// 이동거리 11 => 작동횟수 6
+// 이동거리 12 => 작동횟수 6
+// 이동거리 13 => 작동횟수 7
+// 이동거리 14 => 작동횟수 7
+// 이동거리 15 => 작동횟수 7
--- src/week5/boj_6209.java
+++ src/week5/boj_6209.java
@@ -1,10 +1,32 @@
 package week5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class boj_6209 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int d = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] islands = new int[n];
+		for (int i = 0; i < n; i++) {
+			islands[i] = Integer.parseInt(bf.readLine());
+		}
+		Arrays.sort(islands);
+		
+		int left = 0;
+		int right = d;
+		while(left <= right) {
+			int mid = (left + right) / 2;
+		}
 	}
 
 }
