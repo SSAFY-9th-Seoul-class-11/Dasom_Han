075aee5cdbca76034fdc42cbce7d3c44021b2d22
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-02-27 17:25:26 +0900

    Update boj_11403.java

--- src/week7/boj_11403.java
+++ src/week7/boj_11403.java
@@ -2,8 +2,17 @@ package week7;
 
 public class boj_11403 { // 경로 찾기
 
-	public static void main(String[] args) {
+	static int[][] map;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 	}
-
 }
