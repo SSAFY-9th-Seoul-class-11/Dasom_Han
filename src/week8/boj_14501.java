7be0feb52ee9dc2e964f838e62ae391f3d687c19
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-03-07 11:10:39 +0900

    Update boj_14501.java

--- src/week8/boj_14501.java
+++ src/week8/boj_14501.java
@@ -9,9 +9,8 @@ import java.util.StringTokenizer;
 public class boj_14501 { // 퇴사
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		// 입력
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
 		int N = Integer.parseInt(bf.readLine());
 		int[][] schedule = new int[N][2];
 		for (int i = 0; i < N; i++) {
@@ -20,16 +19,20 @@ public class boj_14501 { // 퇴사
 				schedule[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		int[] maxProfit = new int[N];
+		// dp 테이블
+		int[] maxProfit = new int[N + 1];
 		for (int i = 0; i < N; i++) {
 			int after = schedule[i][0] + i;
-			if (after < N) {
+			if (after <= N) {
 				maxProfit[after] = Math.max(maxProfit[after], maxProfit[i] + schedule[i][1]);
 			}
+
+			maxProfit[i + 1] = Math.max(maxProfit[i], maxProfit[i + 1]); // 다음날 업데이트 해줘야함!!!!
 		}
-		Arrays.sort(maxProfit);
-		System.out.println(maxProfit[N - 1]);
+		// 그 날 상담에서 걸리는 날짜만큼 뒤로 가서 돈을 더해주는데
+		// 만약 이미 그 날짜의 돈이 더 크다면 유지, 아니면 업데이트
+
+		System.out.println(maxProfit[N]); // 퇴사 다음날 들어오는 돈...
 	}
 
 }
