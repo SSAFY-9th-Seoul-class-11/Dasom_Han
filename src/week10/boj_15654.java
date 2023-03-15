d56ca946359edb4953acff2deb0d47825125f156
Amyhds <dasomhan524@gmail.com>
2023-03-15 23:01:22 +0900

    week10 start

---
+++ src/week10/boj_15654.java
@@ -0,0 +1,50 @@
+package week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_15654 {
+	static int N, M;
+	static int[] numbers;
+	static int[] selected;
+	static boolean[] visited;
+	static StringBuilder sb;
+
+	private static void dfs(int cnt) {
+		if (cnt == M) {   // M번 뽑으면 출력
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i] + " ");
+			}
+			sb.append("\n");
+		}
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				selected[cnt] = numbers[i];
+				dfs(cnt + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[N];
+		selected = new int[M];
+		visited = new boolean[N];
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numbers);
+
+		dfs(0);
+	}
+}
