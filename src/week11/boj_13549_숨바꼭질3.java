db6daa2a0356a90a699939573165090a0b74b8ea
Amyhds <dasomhan524@gmail.com>
2023-04-02 20:53:51 +0900

    week11 2/4 complete

--- src/week11/boj_13549_숨바꼭질3.java
+++ src/week11/boj_13549_숨바꼭질3.java
@@ -12,7 +12,7 @@ public class boj_13549_숨바꼭질3 {
 	public static boolean isValid(int loc) {
 		if (loc < 0 || loc > 100000) // 범위를 벗어나거나
 			return false;
-		if (location[loc] != 0) // 그 위치에 이미 다녀갔거나
+		if (location[loc] != -1) // 그 위치에 이미 다녀갔거나
 			return false;
 		return true;
 	}
@@ -28,12 +28,12 @@ public class boj_13549_숨바꼭질3 {
 			location[currLoc] = time;
 			if (currLoc == K)
 				return;
+			if (isValid(currLoc * 2))
+				que.offer(new int[] { currLoc * 2, time });
 			if (isValid(currLoc - 1))
 				que.offer(new int[] { currLoc - 1, time + 1 });
 			if (isValid(currLoc + 1))
 				que.offer(new int[] { currLoc + 1, time + 1 });
-			if (isValid(currLoc * 2))
-				que.offer(new int[] { currLoc * 2, time });
 		}
 
 	}
@@ -44,6 +44,9 @@ public class boj_13549_숨바꼭질3 {
 		N = sc.nextInt(); // 수빈 위치
 		K = sc.nextInt(); // 동생 위치
 		location = new int[100001]; // 수직선
+		for (int i = 0; i < 100001; i++) {
+			location[i] = -1;  // -1로 초기화
+		}
 		bfs();
 		System.out.println(location[K]);
 	}
---
+++ src/week11/boj_13913_숨바꼭질4.java
@@ -0,0 +1,84 @@
+package week11;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class boj_13913_숨바꼭질4 {
+
+	static final int MAX = 100001;
+	static int[] location, parent;
+	static int N, K;
+	static StringBuilder sb;
+
+	private static void printTrace() {
+		Stack<Integer> stack = new Stack<Integer>();
+		int last = K;
+		while (last != N) {
+			stack.push(last);
+			last = parent[last];
+		}
+		stack.push(last);
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop() + " ");
+		}
+	}
+
+	private static boolean isValid(int loc) {
+		if (loc < 0 || loc > 100000)
+			return false;
+		if (location[loc] != 0)
+			return false;
+		return true;
+	}
+
+	private static void find() {
+
+		Queue<Integer> que = new ArrayDeque<Integer>();
+		que.offer(N);
+		int curr = 0;
+		int time = 0;
+		int size = 0;
+		while (!que.isEmpty()) {
+			size = que.size();
+			for (int i = 0; i < size; i++) {
+				curr = que.poll();
+				location[curr] = time;
+				if (curr == K)
+					return;
+				if (isValid(curr - 1)) {
+					que.offer(curr - 1);
+					parent[curr - 1] = curr;
+				}
+				if (isValid(curr + 1)) {
+					que.offer(curr + 1);
+					parent[curr + 1] = curr;
+				}
+				if (isValid(curr * 2)) {
+					que.offer(curr * 2);
+					parent[curr * 2] = curr;
+				}
+			}
+			time++;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		K = sc.nextInt();
+		location = new int[MAX];
+		parent = new int[MAX];
+		sb = new StringBuilder();
+
+		find();
+		System.out.println(location[K]);
+		printTrace();
+		System.out.println(sb.toString());
+	}
+
+}
