7495abf1eff700471a53cb18b9676762490566f7
Amyhds <dasomhan524@gmail.com>
2023-04-18 08:15:21 +0900

    week13 1/4

--- src/week13/Main_2056_작업_한다솜.java
+++ src/week13/Main_2056_작업_한다솜.java
@@ -1,10 +1,72 @@
 package week13;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main_2056_작업_한다솜 {
 
-	public static void main(String[] args) {
-
+	static int N;
+	static int[] inDegree, dp, time;
+	static ArrayList<Integer>[] adjList;
+	
+	public static int topology() {
+		Queue<Integer> que = new ArrayDeque<Integer>();
 		
+		for (int i = 1; i <= N; i++) {
+			dp[i] = time[i];
+			if (inDegree[i] == 0)
+				que.offer(i);
+		}
+		
+		while(!que.isEmpty()) {
+			int curr = que.poll();
+			for (int next : adjList[curr]) {
+				dp[next] = Math.max(dp[next], dp[curr] + time[next]);
+				if (--inDegree[next] == 0)
+					que.offer(next);
+				
+			}
+		}
+		
+		int ans = 0;
+		for (int i = 1; i <= N; i++) {
+			ans = Math.max(ans, dp[i]);
+		}
+		
+		return ans; 
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(bf.readLine());
+		inDegree = new int[N + 1];
+		adjList = new ArrayList[N + 1];
+		time = new int[N + 1];
+		dp = new int[N + 1];
+		for (int i = 0; i <= N; i++) {
+			adjList[i] = new ArrayList<Integer>();
+		}
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			int prev = Integer.parseInt(st.nextToken());
+			if (prev > 0) {
+				for (int j = 0; j < prev; j++) {
+					int preWork = Integer.parseInt(st.nextToken());
+					adjList[preWork].add(i);
+					inDegree[i]++;
+				}
+			}
+		}
+		
+		System.out.println(topology());
 	}
 
 }
--- src/week13/Main_2239_스도쿠_한다솜.java
+++ src/week13/Main_2239_스도쿠_한다솜.java
@@ -1,9 +1,34 @@
 package week13;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main_2239_스도쿠_한다솜 {
+	
+	static int[][] sdk;
+	
+	public static boolean fill(int i, int j, int k) {
+		return false;
+	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		sdk = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			String oneRow = bf.readLine();
+			for (int j = 0; j < 9; j++) {
+				sdk[i][j] = oneRow.charAt(j) - '0';
+			}
+		}
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (sdk[i][j] == 0)
+					fill(i, j);
+			}
+		}
 		
 	}
 
