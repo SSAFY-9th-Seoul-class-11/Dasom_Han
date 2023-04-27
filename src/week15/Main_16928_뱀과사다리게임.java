813a2e36455ee930293c121449aab3facdf549c9
Amyhds <dasomhan524@gmail.com>
2023-04-28 00:15:58 +0900

    week15 2/6 complete

--- src/week15/Main_16928_뱀과사다리게임.java
+++ src/week15/Main_16928_뱀과사다리게임.java
@@ -4,32 +4,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
-class Pos {
-	int x, y;
-
-	public Pos(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-
-}
-
 
 public class Main_16928_뱀과사다리게임 {
 
 	static int N, M;
 	static int[] arr;
 	static int res;
-	
+
 	public static void bfs() {
-		
-		Queue<Pos> que = new ArrayDeque<>();
-		
-		
+
+		Queue<Integer> que = new ArrayDeque<>();
+		que.offer(1);
+		int[] visited = new int[101]; // 방문 체크 및 주사위 굴린 횟수 체크
+		visited[1] = 0;
+
+		while (!que.isEmpty()) {
+			int now = que.poll();
+//			System.out.println(now);
+			if (now == 100) {
+				System.out.println(visited[now]);
+//				System.out.println(Arrays.toString(visited));
+				return;
+			}
+
+			int next;
+			if (arr[now] == now) { // 뱀이나 사다리가 없는 경우 주사위를 굴려 나올 수 있는 모든 곳으로 가본다
+				for (int dice = 1; dice <= 6; dice++) {
+					next = now + dice;
+					if (next > 100)
+						continue;
+					if (visited[next] == 0) {// 가지 않았다면 지금까지의 이동 횟수 + 1 넣기
+						visited[next] = visited[now] + 1;
+					} else {// 간 곳이라면 더 적게 걸린 이동 횟수로 업데이트
+						visited[next] = Math.min(visited[next], visited[now] + 1);
+					}
+					que.offer(next);
+				}
+			} else { // 뱀이나 사다리가 있다면 그곳으로 간다
+				next = arr[now];
+				if (visited[next] == 0) {
+					visited[next] = visited[now]; // 주사위 안굴리고 바로 감
+				} else {
+					visited[next] = Math.min(visited[next], visited[now]);
+				}
+				que.offer(next);
+			}
+		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -39,20 +63,20 @@ public class Main_16928_뱀과사다리게임 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[101];
-		
-		for (int i = 1; i <= 100 ; i++) {
+
+		for (int i = 1; i <= 100; i++) {
 			arr[i] = i;
 		}
-		
+
 		for (int i = 0; i < N + M; i++) {
 			st = new StringTokenizer(bf.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			arr[a] = b;
 		}
-		
+
 		bfs();
-		
+
 	}
 
 }
--- src/week15/Main_17404_RGB거리2.java
+++ src/week15/Main_17404_RGB거리2.java
@@ -3,48 +3,52 @@ package week15;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_17404_RGB거리2 {
-	
+
 	static int N;
 	static int[][] costs, result;
-	
+	static final int R = 0, G = 1, B = 2;
+
 	private static int paint() {
-		// 첫줄과 마지막은 result를 costs와 똑같이 채우기
-		for (int i = 0; i < 3; i++) {
-			result[0][i] = costs[0][i];
-		}
-		// 둘째줄부터 그 전 집과 색이 다르면서 최소인 경우 저장
-		for (int house = 1; house < N; house++) {
-			result[house][0] = costs[house][0] + Math.min(result[house-1][1], result[house-1][2]); // R의 경우 G, B
-			result[house][1] = costs[house][1] + Math.min(result[house-1][0], result[house-1][2]); // G의 경우 R, B
-			result[house][2] = costs[house][2] + Math.min(result[house-1][0], result[house-1][1]); // B의 경우 R, G
-		}
-		// 마지막줄의 최소값
 		int min = Integer.MAX_VALUE;
-		for (int i = 0; i < 3; i++) {
-			min = Math.min(min, result[N-1][i]);
+		// 1번째 집 색을 정해놓고 3가지의 경우를 따진다!
+		for (int setColor = 0; setColor < 3; setColor++) {
+			//1번째 집의 dp 배열 채우기
+			for (int firstColor = 0; firstColor < 3; firstColor++) {
+				if (setColor == firstColor)
+					result[0][firstColor] = costs[0][firstColor];
+				else	
+					result[0][firstColor] = 9999;
+			}
+			// DP 배열 완성
+			for (int house = 1; house < N; house++) {
+				result[house][R] = costs[house][R] + Math.min(result[house - 1][G], result[house - 1][B]); // R의 경우
+				result[house][G] = costs[house][G] + Math.min(result[house - 1][R], result[house - 1][B]); // G의 경우
+				result[house][B] = costs[house][B] + Math.min(result[house - 1][R], result[house - 1][G]); // B의 경우
+			}
+			// 최솟값 갱신
+			for (int lastColor = 0; lastColor < 3; lastColor++) {
+				if (setColor == lastColor)
+					continue;
+				min = Math.min(min, result[N - 1][lastColor]);
+			}
 		}
 		return min;
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
 		costs = new int[N][3];
-		result = new int[N + 1][3];
+		result = new int[N][3];
 		for (int house = 0; house < N; house++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			for (int color = 0; color < 3; color++) {
 				costs[house][color] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for (int house = 0; house <= N; house++) {
-			for (int color = 0; color < 3; color++) {
-				result[house][color] = Integer.MAX_VALUE;
 			}
 		}
 
